8e758b5ebcf362babf01fd7012f4a35d2186ecdd
brainster-one <brainster-one@hotmail.com>
2012-07-27 20:57:17 +0800

    cleaning

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @team_info_article = ArticleDecorator.first()
+    @team_info_article = ArticleDecorator.find_by_id(Settings.team_description_article_id)
   end
 end
